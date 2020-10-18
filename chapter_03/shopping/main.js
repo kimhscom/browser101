8ed27c3ba2e4eec5ff15eca8dc90df1f579636b9
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-10-18 22:01:35 +0900

    #13 JavaScript Dynamic

---
+++ chapter_03/shopping/main.js
@@ -0,0 +1,56 @@
+const items = document.querySelector(".items");
+const input = document.querySelector(".footer__input");
+const addBtn = document.querySelector(".footer__button");
+
+const onAdd = () => {
+  // 1. Obtain text entered by the user
+  const text = input.value;
+  if (text === "") {
+    input.focus();
+    return;
+  }
+  // 2. Create a new item (text + delete Button)
+  const item = createItem(text);
+  // 3. Add the newly created item in the items container.
+  items.appendChild(item);
+  // 4. Initialize input.
+  input.value = "";
+  input.focus();
+};
+
+const createItem = (text) => {
+  const itemRow = document.createElement("li");
+  itemRow.setAttribute("class", "item__row");
+
+  const item = document.createElement("div");
+  item.setAttribute("class", "item");
+
+  const name = document.createElement("span");
+  name.setAttribute("class", "item__name");
+  name.innerText = text;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.setAttribute("class", "item__delete");
+  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  deleteBtn.addEventListener("click", () => {
+    items.removeChild(itemRow);
+  });
+
+  const itemDivider = document.createElement("div");
+  itemDivider.setAttribute("class", "item__divider");
+
+  item.appendChild(name);
+  item.appendChild(deleteBtn);
+
+  itemRow.appendChild(item);
+  itemRow.appendChild(itemDivider);
+  return itemRow;
+};
+
+addBtn.addEventListener("click", () => {
+  onAdd();
+});
+
+input.addEventListener("keypress", (event) => {
+  console.log("key");
+});
