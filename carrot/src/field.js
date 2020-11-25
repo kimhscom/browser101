48e7c41b1170d6208a7f08d7595a1b5719e871e3
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-11-25 22:22:09 +0900

    #30 Create a sound file

--- carrot/src/field.js
+++ carrot/src/field.js
@@ -1,8 +1,8 @@
 "use strict";
 
-const CARROT_SIZE = 80;
+import * as sound from "./sound.js";
 
-const carrotSound = new Audio("./sound/carrot_pull.mp3");
+const CARROT_SIZE = 80;
 
 export default class Field {
   constructor(carrotCount, bugCount) {
@@ -48,7 +48,7 @@ export default class Field {
     if (target.matches(".carrot")) {
       // carrot!!
       target.remove();
-      playSound(carrotSound);
+      sound.playCarrot();
       this.onClick;
       this.onItemClick && this.onItemClick("carrot");
     } else if (target.matches(".bug")) {
@@ -60,9 +60,4 @@ export default class Field {
 
 function randomNumber(min, max) {
   return Math.random() * (max - min) + min;
-}
-
-function playSound(sound) {
-  sound.currentTime = 0;
-  sound.play();
 }
--- carrot/src/main.js
+++ carrot/src/main.js
@@ -2,6 +2,7 @@
 
 import PopUp from "./popup.js";
 import Field from "./field.js";
+import * as sound from "./sound.js";
 
 const CARROT_COUNT = 10;
 const BUG_COUNT = 10;
@@ -10,12 +11,6 @@ const GAME_DURATION_SEC = 20;
 const gameBtn = document.querySelector(".game__button");
 const gameTimer = document.querySelector(".game__timer");
 const gameScore = document.querySelector(".game__score");
-
-const carrotSound = new Audio("./sound/carrot_pull.mp3");
-const alertSound = new Audio("./sound/alert.wav");
-const bgSound = new Audio("./sound/bg.mp3");
-const bugSound = new Audio("./sound/bug_pull.mp3");
-const winSound = new Audio("./sound/game_win.mp3");
 
 let started = false;
 let score = 0;
@@ -61,7 +56,7 @@ function startGame() {
   showStopButton();
   showTimerAndScore();
   startGameTimer();
-  playSound(bgSound);
+  sound.playBackground();
 }
 
 function stopGame() {
@@ -69,17 +64,17 @@ function stopGame() {
   stopGameTimer();
   hideGameButton();
   gameFinishBanner.showWithText("REPLAY❓");
-  playSound(alertSound);
-  stopSound(bgSound);
+  sound.playAlert();
+  sound.stopBackground();
 }
 
 function finishGame(win) {
   started = false;
   hideGameButton();
   if (win) {
-    playSound(winSound);
+    sound.playWin();
   } else {
-    playSound(bugSound);
+    sound.playBug();
   }
   stopGameTimer();
   stopSound(bgSound);
@@ -129,15 +124,6 @@ function initGame() {
   score = 0;
   gameScore.innerText = CARROT_COUNT;
   gameField.init();
-}
-
-function playSound(sound) {
-  sound.currentTime = 0;
-  sound.play();
-}
-
-function stopSound(sound) {
-  sound.pause();
 }
 
 function updateScoreBoard() {
