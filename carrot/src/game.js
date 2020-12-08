ffa3fafdfcc3e051563da8b6f40c8c83ba307d7f
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-12-08 23:08:58 +0900

    #35 Neat function

--- carrot/src/game.js
+++ carrot/src/game.js
@@ -46,7 +46,7 @@ class Game {
     this.gameBtn = document.querySelector(".game__button");
     this.gameBtn.addEventListener("click", () => {
       if (this.started) {
-        this.stop();
+        this.stop(Reason.cancel);
       } else {
         this.start();
       }
@@ -73,26 +73,12 @@ class Game {
     sound.playBackground();
   }
 
-  stop() {
+  stop(reason) {
     this.started = false;
     this.stopGameTimer();
     this.hideGameButton();
-    sound.playAlert();
     sound.stopBackground();
-    this.onGameStop && this.onGameStop(Reason.cancel);
-  }
-
-  finish(win) {
-    this.started = false;
-    this.hideGameButton();
-    if (win) {
-      sound.playWin();
-    } else {
-      sound.playBug();
-    }
-    this.stopGameTimer();
-    sound.stopBackground();
-    this.onGameStop && this.onGameStop(win ? Reason.win : Reason.lose);
+    this.onGameStop && this.onGameStop(reason);
   }
 
   onItemClick = (item) => {
@@ -104,11 +90,11 @@ class Game {
       this.score++;
       this.updateScoreBoard();
       if (this.score === this.carrotCount) {
-        this.finish(true);
+        this.stop(Reason.win);
       }
     } else if (item === "bug") {
       // bug!!
-      this.finish(false);
+      this.stop(Reason.lose);
     }
   };
 
@@ -134,7 +120,7 @@ class Game {
     this.timer = setInterval(() => {
       if (remainingTimeSec <= 0) {
         clearInterval(this.timer);
-        this.finish(this.carrotCount === this.score);
+        this.stop(this.carrotCount === this.score ? Reason.win : Reason.lose);
         return;
       }
       this.updateTimerText(--remainingTimeSec);
--- carrot/src/main.js
+++ carrot/src/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import PopUp from "./popup.js";
+import * as sound from "./sound.js";
 import { GameBuilder, Reason } from "./game.js";
 
 const gameFinishBanner = new PopUp();
@@ -16,12 +17,15 @@ game.setGameStopListener((reason) => {
   switch (reason) {
     case Reason.cancel:
       message = "REPLAY❓";
+      sound.playAlert();
       break;
     case Reason.win:
       message = "YOU WIN 🎉";
+      sound.playWin();
       break;
     case Reason.lose:
       message = "YOU LOSE 💩";
+      sound.playBug();
       break;
     default:
       throw new Error("not valid reason");
